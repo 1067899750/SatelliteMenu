b6f753c8de1ee95e92a1be9969513039a8d6b6e5
diudiuhf <diudiuhf@163.com>
2019-01-06 17:53:07 +0800

    星级菜单

--- app/src/main/java/com/diudiu/satellitemenu/satellite/SatelliteMenuActivity.java
+++ app/src/main/java/com/diudiu/satellitemenu/satellite/SatelliteMenuActivity.java
@@ -3,14 +3,42 @@ package com.diudiu.satellitemenu.satellite;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.diudiu.satellitemenu.R;
+import com.diudiu.satellitemenu.satellite.view.ArcMenu;
 
-public class SatelliteMenuActivity extends Activity {
+public class SatelliteMenuActivity extends Activity implements View.OnClickListener {
+
+    private ArcMenu arcMenu;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statellite_menu);
+
+        initView();
+    }
+
+    private void initView() {
+        arcMenu = findViewById(R.id.arcMenu);
+        findViewById(R.id.btn).setOnClickListener(this);
+        arcMenu.setOnMunuItemClickListener(new ArcMenu.OnMunuItemClickListener() {
+            @Override
+            public void onClick(View view, int pos) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                if (arcMenu.isOpen()) {
+                    arcMenu.toggleMenu(600);
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/diudiu/satellitemenu/satellite/view/ArcMenu.java
+++ app/src/main/java/com/diudiu/satellitemenu/satellite/view/ArcMenu.java
@@ -7,10 +7,16 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 import com.diudiu.satellitemenu.R;
 
-public class ArcMenu extends ViewGroup {
+public class ArcMenu extends ViewGroup implements View.OnClickListener {
 
     private final int POS_LEFT_TOP = 0;
     private final int POS_LEFT_BOTTOM = 1;
@@ -93,12 +99,238 @@ public class ArcMenu extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (changed) {
+            layoutCButton();
+            layoutIButton();
+        }
+    }
 
+    /**
+     * 测量item的位置
+     */
+    private void layoutIButton() {
+        int count = getChildCount();
+        for (int i = 0; i < count - 1; i++) {
+            View child = getChildAt(i + 1);
+
+            int cWidth = child.getMeasuredWidth();
+            int cHeight = child.getMeasuredHeight();
+
+            int cl = (int) (mRadius * Math.sin(Math.PI / 2 / (count - 2) * i));
+            int ct = (int) (mRadius * Math.cos(Math.PI / 2 / (count - 2) * i));
+
+            if (mPosition == Position.LEFT_BOTTOM || mPosition == Position.RIGHT_BOTOTM) {
+                ct = getMeasuredHeight() - ct - cHeight;
+            }
+
+            if (mPosition == Position.RIGHT_BOTOTM || mPosition == Position.RIGHT_TOP) {
+                cl = getMeasuredWidth() - cl - cWidth;
+            }
+
+            child.layout(cl, ct, cl + cWidth, ct + cHeight);
+            child.setVisibility(GONE);
+        }
+    }
+
+    /**
+     * 测量主Button
+     */
+    private void layoutCButton() {
+        mCButton = getChildAt(0);
+        mCButton.setOnClickListener(this);
+
+        int l = 0;
+        int t = 0;
+        int width = mCButton.getMeasuredWidth();
+        int height = mCButton.getMeasuredHeight();
+        switch (mPosition) {
+            case LEFT_TOP:
+                l = 0;
+                t = 0;
+                break;
+            case LEFT_BOTTOM:
+                l = 0;
+                t = getMeasuredHeight() - height;
+                break;
+            case RIGHT_TOP:
+                l = getMeasuredWidth() - width;
+                t = 0;
+                break;
+            case RIGHT_BOTOTM:
+                l = getMeasuredWidth() - width;
+                t = getMeasuredHeight() - height;
+                break;
+        }
+
+        mCButton.layout(l, t, l + width, t + height);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            //测量内部的大小
+            measureChild(getChildAt(i), widthMeasureSpec, heightMeasureSpec);
+        }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        rotateCButton(mCButton, 0, 360, 300);
+
+        toggleMenu(300);
+    }
+
+    /**
+     * 切换菜单
+     *
+     * @param duration
+     */
+    public void toggleMenu(int duration) {
+        int count = getChildCount();
+        for (int i = 0; i < count - 1; i++) {
+            final View childView = getChildAt(i + 1);
+            childView.setVisibility(VISIBLE);
+
+            int cl = (int) (mRadius * Math.sin(Math.PI / 2 / (count - 2) * i));
+            int ct = (int) (mRadius * Math.cos(Math.PI / 2 / (count - 2) * i));
+
+            int xflag = 1;
+            int yflag = 1;
+
+            if (mPosition == Position.LEFT_TOP || mPosition == Position.LEFT_BOTTOM) {
+                xflag = -1;
+            }
+            if (mPosition == Position.RIGHT_TOP || mPosition == Position.LEFT_TOP) {
+                yflag = -1;
+            }
+            AnimationSet animationSet = new AnimationSet(true);
+            TranslateAnimation translateAnimation = null;
+            //打开
+            if (mCurrentStatus == Status.CLOSE) {
+                translateAnimation = new TranslateAnimation(cl * xflag, 0, ct * yflag, 0);
+                childView.setFocusable(true);
+                childView.setEnabled(true);
+            } else {//关闭
+                translateAnimation = new TranslateAnimation(0, cl * xflag, 0, ct * yflag);
+                childView.setEnabled(false);
+                childView.setFocusable(false);
+            }
+
+            translateAnimation.setDuration(duration);
+            translateAnimation.setFillAfter(true);
+            RotateAnimation rotateAnimation = new RotateAnimation(0, 720, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotateAnimation.setDuration(duration);
+            rotateAnimation.setFillAfter(true);
+            animationSet.addAnimation(rotateAnimation);
+            animationSet.addAnimation(translateAnimation);
+            animationSet.setStartOffset(i * 100 / count);
+            childView.startAnimation(animationSet);
+
+            animationSet.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    if (mCurrentStatus == Status.CLOSE) {
+                        childView.setVisibility(GONE);
+                    }
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            final int pos = i + 1;
+
+            childView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onMunuItemClickListener != null) {
+                        onMunuItemClickListener.onClick(childView, pos);
+                        onMenuItemAnimate(pos - 1);
+                        changeIStatus();
+                    }
+                }
+            });
+        }
+        changeIStatus();
+    }
+
+    /**
+     * @param pos
+     */
+    private void onMenuItemAnimate(int pos) {
+        for (int i = 0; i < getChildCount() - 1; i++) {
+            View childView = getChildAt(i + 1);
+            if (pos == i) {
+                childView.startAnimation(onMenuBigAnimate(300));
+            } else {
+                childView.startAnimation(onMenuSmallAnimate(300));
+            }
+        }
+    }
+
+    /**
+     * 缩小的动画
+     *
+     * @param duration
+     * @return
+     */
+    private Animation onMenuSmallAnimate(int duration) {
+        AnimationSet animationSet = new AnimationSet(true);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.0f, 0f, 1.0f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0f);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.setDuration(duration);
+        animationSet.setFillAfter(true);
+        return animationSet;
+    }
+
+    /**
+     * 放大的动画
+     *
+     * @param duration
+     * @return
+     */
+    private Animation onMenuBigAnimate(int duration) {
+        AnimationSet animationSet = new AnimationSet(true);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.0f, 4.0f, 1.0f, 4.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0f);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.setDuration(duration);
+        animationSet.setFillAfter(true);
+        return animationSet;
+    }
+
+    /**
+     * 改变item打开状态
+     */
+    private void changeIStatus() {
+        mCurrentStatus = (mCurrentStatus == Status.CLOSE) ? Status.OPEN : Status.CLOSE;
+    }
+
+    private void rotateCButton(View view, float start, float end, int duration) {
+        RotateAnimation rotateAnimation = new RotateAnimation(start, end, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(duration);
+        rotateAnimation.setFillAfter(true);
+        view.startAnimation(rotateAnimation);
+    }
+
+    /**
+     * 打开状态的判定
+     * @return
+     */
+    public boolean isOpen() {
+        return mCurrentStatus == Status.OPEN;
     }
 }
